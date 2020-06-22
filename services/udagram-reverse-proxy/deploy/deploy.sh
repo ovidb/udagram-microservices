bdada39a506dffc9067ecbb5b9bc3c018665cc15
ovidb <antarexis@gmail.com>
2020-06-22 12:17:51 +0100

    Fix environment variables injection

--- services/udagram-reverse-proxy/deploy/deploy.sh
+++ services/udagram-reverse-proxy/deploy/deploy.sh
@@ -9,7 +9,6 @@ export AWS_REGION_BASE64=`echo -n "$AWS_REGION" | base64`
 export AWS_PROFILE_BASE64=`echo -n "$AWS_PROFILE" | base64`
 export JWT_SECRET_BASE64=`echo -n "$JWT_SECRET" | base64`
 export URL_BASE64=`echo -n "$URL" | base64`
-export AWS_CREDENTIALS_FILE_BASE64=`cat $HOME/.aws/credentials | base64 -w 0`
 
 cat env-variables.yaml | envsubst | kubectl apply -f -
 kubectl apply -f ./deployment.yaml
